393e617441c9677f52f3137ade71729242efe63a
KingS0l0m0n <149574957+KingS0l0m0n@users.noreply.github.com>
2024-05-27 16:52:30 +0200

    Connection method added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module org.example.booking {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens org.example.booking to javafx.fxml;
     exports org.example.booking;
--- src/main/java/org/example/booking/databaseAccess.java
+++ src/main/java/org/example/booking/databaseAccess.java
@@ -1,4 +1,22 @@
 package org.example.booking;
 
-public class databaseAccess {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class databaseAccess {    public static Connection getConnection() throws SQLException {
+    Connection connection = null;
+    try {
+        // Load the JDBC driver class
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+        // Establish the connection
+        connection = DriverManager.getConnection("JDBC", "USERNAME", "PASSWORD");
+        System.out.println("Connected to the database.");
+    } catch (ClassNotFoundException e) {
+        System.out.println("Error: SQL Server JDBC Driver not found.");
+        e.printStackTrace();
+    }
+    return connection;
+}
 }
